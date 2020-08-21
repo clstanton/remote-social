1ba6c41985dfb342bbc585d0bb050fc982841ead
Conrad Trost <ctrost@tamu.edu>
2020-08-20 19:30:04 -0500

    merge

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,19 +7,19 @@ class Navigation extends React.Component {
     render() {
         return (
             <Container>
-            <Navbar expand="lg" fixed="top" className="nav-bar ml-auto" bg="transparent" >
-            <Navbar.Brand href="/">Remote Social</Navbar.Brand>
-  
-              <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-              <Navbar.Collapse id="navbar-toggle">
-                
-                  <Nav className="ml-auto">
-                    <Link className="nav-link" to="/about">Browse</Link>
-                    <Link className="nav-link" to="/projects">Library</Link>
-                    <Link className="nav-link" to="/resume">Account</Link>
-                  </Nav>
-              </Navbar.Collapse>
-            </Navbar>
+              <Navbar expand="lg" fixed="top" className="nav-bar ml-auto" bg="transparent" >
+              <Navbar.Brand href="/">Remote Social</Navbar.Brand>
+    
+                <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+                <Navbar.Collapse id="navbar-toggle">
+                  
+                    <Nav className="ml-auto">
+                      <Link className="nav-link" to="/about">Browse</Link>
+                      <Link className="nav-link" to="/projects">Library</Link>
+                      <Link className="nav-link" to="/resume">Account</Link>
+                    </Nav>
+                </Navbar.Collapse>
+              </Navbar>
             </Container>
         );
     }
