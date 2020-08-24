31438a7d79365712e554c30ce0762b98ebb50241
clstanton <cstanton77@gmail.com>
2020-08-24 07:50:17 -0500

    client-side query updates made

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SearchMovies from './pages/SearchMovies';
 import SavedMovies from './pages/SavedMovies';
+import SingleComment from './pages/SingleComment';
+import NoMatch from './pages/NoMatch';
 import './App.scss';
+//import Profile from './pages/Profile';
+//import Signup from './pages/Signup';
+//import Login from './pages/Login';
 
 const client = new ApolloClient({
   request: operation => {
@@ -28,8 +33,12 @@ function App() {
             <Navbar />
             <Switch>
               <Route exact path='/' component={SearchMovies} />
-              <Route exact path='/saved' component={SavedMovies} />
+              {/*<Route exact path='/saved' component={SavedMovies} />*/}
+              <Route exact path="/saved/:username?" component={SavedMovies} />
+              <Route exact path="/comment/:id" component={SingleComment} />
               <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+
+              <Route component={NoMatch} />
             </Switch>
           </>
         </Router>
